83bc47565b73c327a23d26d34b494d8191903906
shukumar-542 <shukumar542@gmail.com>
2024-07-04 12:12:34 +0600

    configure MUI theme provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 
 const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <Button variant='contained'>Contained</Button>
     </div>
   );
 };
